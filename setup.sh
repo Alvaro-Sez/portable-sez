4cf5013c4b618be3ad81ab8022f4bbbd384e11ca
sezif <arrovaalvaro@gmail.com>
2024-06-22 17:42:41 +0200

    update: Sat Jun 22 17:42:41 CEST 2024

--- setup.sh
+++ setup.sh
@@ -41,5 +41,24 @@ cargo install alacritty
 echo "Alacritty Installed"
 
 #---- Configuration ----#
-chmod +x ./configure.sh
-./configure.sh
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+# create if not exists
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/alacritty
+
+cp $SCRIPT_DIR/.zshrc $HOME/.zshrc 
+cp $SCRIPT_DIR/.ideavimrc $HOME/.ideavimrc 
+cp $SCRIPT_DIR/.tmux.conf $HOME/.tmux.conf 
+cp $SCRIPT_DIR/init.vim $HOME/.config/nvim/init.vim  
+cp $SCRIPT_DIR/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+
+# git scripts path
+export PATH="$PATH:/$SCRIPT_DIR/gitScripts"
+
+#Install vim pluggins
+nvim -es -u $HOME/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "qa"
+chsh -s zsh
+echo "Configuration applied"
+
+# cleaning path
